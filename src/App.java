8cd29557164e5c1572d0dddcac570d42209a2ee0
Joao Pedro Linhares <Joo Pedro Linhares>
2025-02-18 16:22:03 -0300

    feat: add arrays of favorites and reviews

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import static classes.Bloco.calculateEstimateTotalInCarnaval;
+import static classes.Bloco.listEstimatePublic;
+
 import classes.*;
 
 public class App {
@@ -6,18 +9,27 @@ public class App {
     AgendaDeBlocos agenda = new AgendaDeBlocos();
 
     BlocoTematico blocoT = new BlocoTematico(
-      "Bloco Louco",
-      "FUNK",
-      "Rio de Janeiro",
+      "Samba do João - Pólo São Gonçalo",
+      "Samba",
+      "Copacabana",
       "15/02/2025 20:00:00",
       100,
       "Hallow"
     );
 
+    BlocoTradicional blocoTrad = new BlocoTradicional(
+      "MBP do João - Polo São Gonçalo",
+      "MBP",
+      "Copacabana",
+      "15/02/2025 20:00:00",
+      350,
+      25
+    );
+
     BlocoInfatil BlocoI = new BlocoInfatil(
-      "Infantil",
-      "BTS",
-      "Rio de Janeiro",
+      "Forró do João - Pólo São Gonçalo",
+      "Forró",
+      "Barra da Tijuca",
       "15/02/2025 20:00:00",
       500,
       "+12"
@@ -25,8 +37,23 @@ public class App {
 
     agenda.registerBlock(blocoT);
     agenda.registerBlock(BlocoI);
+    agenda.registerBlock(blocoTrad); //Bloco adicionado no mesmo horario
 
-    System.out.println("Data e hora: " + blocoT.getDateHour());
-    System.out.println("Data e Hora: " + BlocoI.getDateHour());
+    Foliao foliao1 = new Foliao(
+      "Folião João - Pólo São Gonçalo",
+      26,
+      "Funk",
+      "Barra da Tijuca"
+    );
+
+    foliao1.reviewBlock(BlocoI, 5);
+
+    foliao1.favoriteBlock(BlocoI);
+
+    foliao1.recommendBlocks(agenda.getBlocks());
+
+    listEstimatePublic(agenda.getBlocks());
+
+    calculateEstimateTotalInCarnaval(agenda.getBlocks());
   }
 }
--- src/classes/Bloco.java
+++ src/classes/Bloco.java
@@ -2,6 +2,8 @@ package classes;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Bloco {
 
@@ -10,6 +12,8 @@ public abstract class Bloco {
   private String neighborhood;
   private LocalDateTime dateHour;
   private int numPartygoersLastYear;
+  private List<Foliao> favorites = new ArrayList<>();
+  private List<Integer> reviews = new ArrayList<>();
 
   private static final DateTimeFormatter FORMATTER =
     DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"); //Conversor de horario(String) para LocalDateTime
@@ -66,5 +70,93 @@ public abstract class Bloco {
 
   public void setNumPartygoersLastYear(int numPartygoersLastYear) {
     this.numPartygoersLastYear = numPartygoersLastYear;
+  }
+
+  public void setFavorite(Foliao foliao) {
+    if (!this.favorites.contains(foliao)) {
+      System.out.println(
+        "Favorito Adicionado com Sucesso na conta do foliao: " +
+        foliao.getName()
+      );
+      favorites.add(foliao);
+    } else {
+      System.out.println(
+        foliao.getName() + ", você já adicionou esse bloco no seu favoritos."
+      );
+    }
+  }
+
+  public static List<Bloco> sortBlockByPublic(List<Bloco> blocos) {
+    // Aqui irei usar o algoritmo Bubble Sort
+    for (int i = 0; i < blocos.size() - 1; i++) {
+      for (int j = 0; j < blocos.size() - i - 1; j++) {
+        if (
+          blocos.get(j).calculateEstimetePublic() <
+          blocos.get(j + 1).calculateEstimetePublic()
+        ) {
+          Bloco temp = blocos.get(j);
+          blocos.set(j, blocos.get(j + 1));
+          blocos.set(j + 1, temp);
+        }
+      }
+    }
+    return blocos;
+  }
+
+  public static void calculateEstimateTotalInCarnaval(List<Bloco> blocos) {
+    int sum = 0;
+    for (int i = 0; i < blocos.size() - 1; i++) {
+      sum += blocos.get(i).calculateEstimetePublic();
+    }
+    System.out.println(
+      "A estimativa de público total do Carnaval no Rio de Janeiro é: " + sum
+    );
+  }
+
+  public static void listEstimatePublic(List<Bloco> blocos) {
+    List<Bloco> ordered = sortBlockByPublic(blocos);
+    System.out.println(
+      "\nVeje a estatistica de públicos no blocos logo abaixo:"
+    );
+    for (Bloco bloco : ordered) {
+      System.out.println(
+        bloco.getName() + " - " + bloco.calculateEstimetePublic() + " pessoas"
+      );
+    }
+    System.out.println();
+  }
+
+  //0 <= x <= 6
+  public void setReview(int rating) {
+    reviews.add(rating);
+  }
+
+  public List<Integer> getReviwes() {
+    return this.reviews;
+  }
+
+  // Retorna média das notas dadas
+  public float getReviewsAverage() {
+    if (this.reviews.isEmpty()) return 0;
+    int sum = 0;
+    for (int review : this.reviews) {
+      sum += review;
+    }
+    return (float) sum / this.reviews.size();
+  }
+
+  public int calculateEstimetePublic() {
+    double Am = getReviewsAverage();
+    int Nf = favorites.size();
+    int Fa = numPartygoersLastYear;
+    return (int) ((Nf * 2) + (Am * Fa * 0.2) + (Fa * 0.8));
+  }
+
+  public List<Foliao> getFavorites() {
+    return this.favorites;
+  }
+
+  public int getFavoritesInt() {
+    return this.favorites.size();
   }
 }
